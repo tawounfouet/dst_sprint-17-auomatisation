1c4fb3736d77cf2220227409d67db039bdc40fac
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-06 09:22:28 +0200

    feat(terraform-final): add RDS module outputs

---
+++ 192 - Terraform DevOps/Projet_Final/wordpress-aws/modules/rds/outputs.tf
@@ -0,0 +1,15 @@
+output "endpoint" {
+  value = aws_db_instance.this.address
+}
+
+output "port" {
+  value = aws_db_instance.this.port
+}
+
+output "db_name" {
+  value = aws_db_instance.this.db_name
+}
+
+output "master_user_secret_arn" {
+  value = aws_db_instance.this.master_user_secret[0].secret_arn
+}
