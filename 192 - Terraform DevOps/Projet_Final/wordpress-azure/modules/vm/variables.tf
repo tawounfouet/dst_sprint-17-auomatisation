fc121fcce23f2721473865d7f473ec7210ea06d4
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-06 09:52:31 +0200

    feat(terraform-azure): add VM module variables

---
+++ 192 - Terraform DevOps/Projet_Final/wordpress-azure/modules/vm/variables.tf
@@ -0,0 +1,21 @@
+variable "project_name" { type = string }
+variable "environment" { type = string }
+variable "resource_group" { type = string }
+variable "location" { type = string }
+variable "subnet_id" { type = string }
+variable "network_security_group_id" { type = string }
+variable "vm_size" { type = string }
+variable "zone" { type = string }
+variable "admin_username" { type = string }
+variable "ssh_public_key" {
+  type      = string
+  sensitive = true
+}
+variable "mysql_fqdn" { type = string }
+variable "mysql_database_name" { type = string }
+variable "mysql_admin_username" { type = string }
+variable "key_vault_id" { type = string }
+variable "key_vault_uri" { type = string }
+variable "db_secret_name" { type = string }
+variable "enable_https" { type = bool }
+variable "tags" { type = map(string) }
