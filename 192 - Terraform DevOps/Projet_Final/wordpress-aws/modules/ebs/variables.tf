4e8baa5e7177a3e8fba288dd85e2445631d1cad7
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-06 09:23:31 +0200

    feat(terraform-final): add EBS module variables

---
+++ 192 - Terraform DevOps/Projet_Final/wordpress-aws/modules/ebs/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = string
+}
+
+variable "instance_id" {
+  type = string
+}
+
+variable "volume_size" {
+  type = number
+}
